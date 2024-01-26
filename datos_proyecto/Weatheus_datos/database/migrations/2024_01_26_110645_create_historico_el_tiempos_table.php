d06e5aa0c5336b804d95fe5f809465ff900722e2
Alejandro <ikcev@plaiaundi.net>
2024-01-26 13:11:03 +0100

    Migraciones y Modelos

---
+++ datos_proyecto/Weatheus_datos/database/migrations/2024_01_26_110645_create_historico_el_tiempos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('historico_el_tiempos', function (Blueprint $table) {
+            $table->id();
+            $table->string('estadoCielo')->nullable();
+            $table->int('precipitacion')->nullable();
+            $table->float('temp')->nullable();
+            $table->int('tempMax')->nullable();
+            $table->int('tempMin')->nullable();
+            $table->int('humedad')->nullable();
+            $table->int('sensTermica')->nullable();
+            $table->string('dirViento')->nullable();
+            $table->int('velViento')->nullable();
+            $table->int('idMunicipio')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('historico_el_tiempos');
+    }
+};
